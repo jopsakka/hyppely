1d5ad609b96cc0896906c2d335949b1c5ccbe0f3
jopsakka <jopi.hetemaki@gmail.com>
2015-11-20 19:18:37 +0200

    moi

--- hyppely/src/main/java/jopi/hyppely/jutskat/Pelaaja.java
+++ hyppely/src/main/java/jopi/hyppely/jutskat/Pelaaja.java
@@ -4,12 +4,11 @@ package jopi.hyppely.jutskat;
 
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.Rectangle;
 import java.awt.event.KeyEvent;
 import jopi.hyppely.main.GamePanel;
 
 
-public class Pelaaja extends Rectangle{
+public class Pelaaja{
     private static final long serialVersionUID = 1L;
     
     private boolean oikea = false, vasen = false, ylos = false, alas = false;
@@ -27,9 +26,9 @@ public class Pelaaja extends Rectangle{
         y = GamePanel.HEIGHT / 2;
         xSpeed = 0;
         ySpeed = 0;
-        maxSpeed = 100;
-        kitka = (float) 0.05;
-        acceleration = (float) 0.1;
+        maxSpeed = 30;
+        kitka = (float) 0.1;
+        acceleration = (float) 2;
         this.width = width;
         this.height = height;
     }
@@ -47,17 +46,39 @@ public class Pelaaja extends Rectangle{
         if (alas) {
             ySpeed += acceleration;
         }
-        if ((!vasen && !oikea) && (!ylos && !alas)) {
+        //if ((!vasen && !oikea) && (!ylos && !alas)) {
             ySpeed = ySpeed - (ySpeed * kitka);
             xSpeed = xSpeed - (xSpeed * kitka);
+        //}
+        if (xSpeed > maxSpeed) {
+            xSpeed = maxSpeed;
+        } if (xSpeed < (maxSpeed * -1)) {
+            xSpeed = maxSpeed * -1;
+        } if (ySpeed > maxSpeed) {
+            ySpeed = maxSpeed;
+        } if (ySpeed < maxSpeed * -1) {
+            ySpeed = maxSpeed * -1;
         }
         x += xSpeed;
         y += ySpeed;
+        if (x < 0) {
+            x = 0;
+            xSpeed = xSpeed * -1;
+        } if ( x > GamePanel.WIDTH - width) {
+            x = GamePanel.WIDTH - width;
+            xSpeed = xSpeed * -1;
+        } if (y < 0) {
+            y = 0;
+            ySpeed = ySpeed * -1;
+        } if (y > GamePanel.HEIGHT - height) {
+            y = GamePanel.HEIGHT - height;
+            ySpeed = ySpeed * -1;
+        }
     }
     
     public void draw(Graphics g) {
         g.setColor(Color.BLACK);
-        g.fillOval(Math.round(x), Math.round(y), width, height);
+        g.drawRect(Math.round(x), Math.round(y), width, height);
         //g.fillRect(Math.round(x), Math.round(y), width, height);
     }
     
@@ -90,4 +111,44 @@ public class Pelaaja extends Rectangle{
             alas = false;
         }
     }
+    public int getX() {
+        return Math.round(x);
+    }
+    public int getY() {
+        return Math.round(y);
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public float getxSpeed() {
+        return xSpeed;
+    }
+
+    public void setxSpeed(float xSpeed) {
+        this.xSpeed = xSpeed;
+    }
+
+    public float getySpeed() {
+        return ySpeed;
+    }
+
+    public void setySpeed(float ySpeed) {
+        this.ySpeed = ySpeed;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+    
+   
 }
--- hyppely/src/main/java/jopi/hyppely/pelitilat/Taso1.java
+++ hyppely/src/main/java/jopi/hyppely/pelitilat/Taso1.java
@@ -2,12 +2,16 @@
 package jopi.hyppely.pelitilat;
 
 import java.awt.Graphics;
+import jopi.hyppely.jutskat.Este;
 import jopi.hyppely.jutskat.Pelaaja;
+import jopi.hyppely.jutskat.Tormays;
 
 
 public class Taso1 extends PeliTila{
     
     private Pelaaja pelaaja;
+    private Este este;
+    
 
     public Taso1(PeliTilaHallinta pth) {
         super(pth);
@@ -15,17 +19,25 @@ public class Taso1 extends PeliTila{
 
     
     public void init() {
-        pelaaja = new Pelaaja(30, 30);
+        pelaaja = new Pelaaja(100, 30);
+        este = new Este(400, 100, 0, 0);
     }
 
     
     public void tick() {
         pelaaja.tick();
+        este.tick();
+        
+        if(Tormays.tormaako(pelaaja, este)) {
+            Tormays.tormays(pelaaja, este);
+        }
+        
     }
 
     
     public void draw(Graphics g) {
         pelaaja.draw(g);
+        este.draw(g);
     }
 
     
